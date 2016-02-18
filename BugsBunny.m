37c00a2d83eecbe53ef7362aed46ab444c11ccdc
Sompob Saralamba <sakngoi@hotmail.com>
2016-02-18 12:57:08 +0700

    Update BugsBunny.m

--- BugsBunny.m
+++ BugsBunny.m
@@ -5,6 +5,7 @@
 BeginPackage["BugsBunny`"]
 (* Exported symbols added here with SymbolName::usage *) 
 BugsCH::usage="BugsCH[codaCHfile,codaIndexfile,obj] extracts an MCMC chain of obj from codaCHfile and codaindexfile."
+BugsCHzip::usage="BugsCHzip[zipfile,subfolder,obj] extracts an MCMC chain of obj from subfolder<>coda1.txt."
 BugsObjList::usage="BugsObjList[codaIndexfile] gives the list of the objects in codaIndexfile."
 
 Begin["`Private`"]
@@ -35,6 +36,21 @@ BugsCH[codaCHfile_,codaIndexfile_,obj_]:=Module[{codaCH,codaobj,posindx},
 	(*codaobj=ToExpression@codaCH[[#1;;#2,2]]&@@posindx;*)
 	codaobj=ToExpression@StringReplace[codaCH[[#1;;#2,2]]&@@posindx,"E"->"*^"];
 	codaobj
+];
+
+(*for using with the zip file all files in the WinBUGS output folder; 
+now it only works with Chain 1; that is codaCHfile = "coda1.txt" and codaIndexfile = "codaIndex.txt"
+*)
+BugsCHzip[zipfile_,subfolder_,obj_]:=Module[{codaIndex, codaCH, posIndx, codaobj},
+	codaIndex = Partition[StringSplit[Import[zipfile,subfolder<>"\\codaIndex.txt"]],3];
+	codaCH = Partition[StringSplit[Import[zipfile, subfolder<>"\\coda1.txt"]],2];
+	
+	(* find the position of the object *)
+	posIndx=ToExpression@(codaIndex[[Position[codaIndex,obj]//Flatten//First]]//Rest);
+	
+	codaobj=ToExpression@StringReplace[codaCH[[#1;;#2,2]]&@@posIndx,"E"->"*^"];
+	codaobj
+
 ];
 
 BugsObjList[codaIndexfile_]:=Module[{codaIndex,objlist},
